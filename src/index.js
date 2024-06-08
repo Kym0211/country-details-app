75e5f6e31a5767be7fb8307214ed770b8d7901f4
Kavyam <kavyam.ksingh@gmail.com>
2024-06-08 17:55:05 +0530

    SpeedInsights added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import App from './app/js/app';
 import './app/scss/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
+        <SpeedInsights />
     </StrictMode>
 );
